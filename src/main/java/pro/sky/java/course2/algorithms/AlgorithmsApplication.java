1e7aad9a7937bc74fa2e714e5e2bf3fb5dc7975b
Vadim <manjavha@gmail.com>
2022-06-15 00:50:39 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/algorithms/AlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/algorithms/AlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
