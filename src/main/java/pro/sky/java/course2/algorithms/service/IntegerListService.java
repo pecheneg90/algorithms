c91db613dd6ccb11ca6c18e8553d972daed406cc
Vadim <manjavha@gmail.com>
2022-06-19 12:43:21 +0300

    homework 16 "algorithms pt.2" - first commit!

---
+++ src/main/java/pro/sky/java/course2/algorithms/service/IntegerListService.java
@@ -0,0 +1,4 @@
+package pro.sky.java.course2.algorithms.service;
+
+public class IntegerListService {
+}
---
+++ src/main/java/pro/sky/java/course2/algorithms/service/impl/IntegerListServiceImpl.java
@@ -0,0 +1,4 @@
+package pro.sky.java.course2.algorithms.service.impl;
+
+public class IntegerListServiceImpl {
+}
